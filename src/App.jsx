95ac418b1c184bc563df5c14b717a5cdf7d960f0
SHS-Peb <blackskiesdancer@gmail.com>
2026-01-03 23:46:46 +1100

    background added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './index.css'
+import treeBg from "./assets/imgs/tree.png";
 
 function App() {
 
   return (
     <>
-      
+      <main
+      className="min-h-screen w-full bg-cover bg-center bg-no-repeat relative"
+      style={{ backgroundImage: `url(${treeBg})` }}
+    ></main>
     </>
   )
 }
